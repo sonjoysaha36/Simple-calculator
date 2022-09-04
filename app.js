1963a4e5bdf461a2858cce1d7557cfc956f87e9c
sonjoysaha36 <sonjoysaha36@gmail.com>
2022-09-04 19:38:18 +0600

    javaScript file

---
+++ app.js
@@ -0,0 +1,18 @@
+let result = document.getElementById("inputext");
+
+let calculate =(number)=>{
+    result.value +=number;
+}
+
+let Result =()=>{
+    try{
+        result.value = eval(result.value)
+    }
+    catch(err){
+        alert("Enter the valid Input");
+    }
+}
+
+function clr(){
+    result.value ="";
+}
